b2249476481da8d1f6d52f876d577c2497124ab3
Pedro Henrique Durand Reis <webpedrod@gmail.com>
2022-06-02 16:09:16 -0300

    Feat: adicionado campo para novas postagens e lista de posts

--- modulo1/estados-insta4/insta4/src/App.js
+++ modulo1/estados-insta4/insta4/src/App.js
@@ -1,35 +1,94 @@
-import React from 'react';
-import styled from 'styled-components'
-import Post from './components/Post/Post';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Post from "./components/Post/Post";
 
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-`
+`;
+
 function App() {
-return(
-  <MainContainer>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-          <Post
-            nomeUsuario={'pepedurand'}
-            fotoUsuario={'https://picsum.photos/50/30'}
-            fotoPost={'https://picsum.photos/200/100'}
-          />
-          <Post
-            nomeUsuario={'southamericamemes'}
-            fotoUsuario={'https://picsum.photos/50/20'}
-            fotoPost={'https://picsum.photos/200/90'}
-          />
-        </MainContainer>
-)
+  const [posted, setPosted] = useState({
+    posts: [
+      {
+        nomeUsuario: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150",
+      },
+      {
+        nomeUsuario: "pepedurand",
+        fotoUsuario: "https://picsum.photos/50/30",
+        fotoPost: "https://picsum.photos/200/100",
+      },
+      {
+        nomeUsuario: "southamericamemes",
+        fotoUsuario: "https://picsum.photos/50/20",
+        fotoPost: "https://picsum.photos/200/90",
+      },
+    ],
+  });
+  const [valorInputUser, setValorInputUser] = useState("");
+  const [valorInputUserFotoPerfil, setValorInputUserFotoPerfil] = useState("");
+  const [valorInputUserFoto, setValorInputUserFoto] = useState("");
 
+  const adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: valorInputUser,
+      fotoUsuario: valorInputUserFotoPerfil,
+      fotoPost: valorInputUserFoto,
+    };
+    const postNovo = [...posted.posts, novoPost];
+    const novoEstado = {
+      posts: postNovo,
+    };
+    setPosted(novoEstado);
+  };
+
+  const onChangeInputUser = (event) => {
+    setValorInputUser(event.target.value);
+  };
+  const onChangeInputUserFotoPerfil = (event) => {
+    setValorInputUserFotoPerfil(event.target.value);
+  };
+  const onChangeInputUserFoto = (event) => {
+    setValorInputUserFoto(event.target.value);
+  };
+
+  const listaPosts = posted.posts.map((post) => {
+    return (
+      <div>
+        <Post
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
+      </div>
+    );
+  });
+
+  return (
+    <MainContainer>
+      <input
+        value={valorInputUser}
+        onChange={onChangeInputUser}
+        placeholder={"User"}
+      ></input>
+      <input
+        value={valorInputUserFotoPerfil}
+        onChange={onChangeInputUserFotoPerfil}
+        placeholder={"URL foto perfil"}
+      ></input>
+      <input
+        value={valorInputUserFoto}
+        onChange={onChangeInputUserFoto}
+        placeholder={"URL foto post"}
+      ></input>
+      <button onClick={adicionaPost}>Novo Post</button>
+      <div>{listaPosts}</div>
+    </MainContainer>
+  );
 }
-
 
 export default App;
--- modulo1/estados-insta4/insta4/src/components/Post/Post.js
+++ modulo1/estados-insta4/insta4/src/components/Post/Post.js
@@ -1,25 +1,25 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import {IconeComContador} from '../IconeComContador/IconeComContador'
+import { IconeComContador } from "../IconeComContador/IconeComContador";
 
-import iconeCoracaoBranco from '../../img/favorite-white.svg'
-import iconeCoracaoPreto from '../../img/favorite.svg'
-import iconeComentario from '../../img/comment_icon.svg'
-import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import iconeCoracaoBranco from "../../img/favorite-white.svg";
+import iconeCoracaoPreto from "../../img/favorite.svg";
+import iconeComentario from "../../img/comment_icon.svg";
+import { SecaoComentario } from "../SecaoComentario/SecaoComentario";
 
 const PostContainer = styled.div`
   border: 1px solid gray;
   width: 300px;
   margin-bottom: 10px;
-`
+`;
 
 const PostHeader = styled.div`
   height: 40px;
   display: flex;
   align-items: center;
   padding-left: 10px;
-`
+`;
 
 const PostFooter = styled.div`
   height: 40px;
@@ -27,85 +27,73 @@ const PostFooter = styled.div`
   align-items: center;
   padding: 0 10px;
   justify-content: space-between;
-`
+`;
 
 const UserPhoto = styled.img`
   height: 30px;
   width: 30px;
   margin-right: 10px;
   border-radius: 50%;
-`
+`;
 
 const PostPhoto = styled.img`
   width: 100%;
-`
+`;
 
-function Post(props){
-  const [state, setState] = useState({
-    curtido: false,
-    numeroCurtidas: 0,
-    comentando: false,
-    numeroComentarios: 0
-  })
-
-  const [numeroCurtidas, setnumeroCurtidas] = useState (0)
-  const [curtido, setCurtido] = useState(false)
-  const [comentando, setComentando] = useState(false)
-  const [numeroComentarios, setNumeroComentarios] = useState(0)
-
-
+function Post(props) {
+  const [numeroCurtidas, setnumeroCurtidas] = useState(0);
+  const [curtido, setCurtido] = useState(false);
+  const [comentando, setComentando] = useState(false);
+  const [numeroComentarios, setNumeroComentarios] = useState(0);
 
   const contadorCurtida = () => {
-    if(!curtido){
-    setnumeroCurtidas(numeroCurtidas +1)
-  }
-    else{
-      setnumeroCurtidas(numeroCurtidas -1)
+    if (!curtido) {
+      setnumeroCurtidas(numeroCurtidas + 1);
+    } else {
+      setnumeroCurtidas(numeroCurtidas - 1);
     }
-  }
+  };
 
   const onClickCurtida = () => {
-    setCurtido(!curtido)
-    contadorCurtida()
-  }
-  
+    setCurtido(!curtido);
+    contadorCurtida();
+  };
 
-  
   const onClickComentario = () => {
-    setComentando(!comentando)
-    if(comentando) {
-      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
+    setComentando(!comentando);
+    if (comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario} />;
     }
-    console.log(comentando)
-  }
-  
+    console.log(comentando);
+  };
+
   const aoEnviarComentario = () => {
-    setComentando(false)
-    setNumeroComentarios(numeroComentarios + 1)
+    setComentando(false);
+    setNumeroComentarios(numeroComentarios + 1);
+  };
+
+  let iconeCurtida;
+
+  if (curtido) {
+    iconeCurtida = iconeCoracaoPreto;
+  } else {
+    iconeCurtida = iconeCoracaoBranco;
   }
 
-  let iconeCurtida
+  let componenteComentario;
 
-    if(curtido) {
-      iconeCurtida = iconeCoracaoPreto
-    } else {
-      iconeCurtida = iconeCoracaoBranco 
-    }
+  if (comentando) {
+    componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario} />;
+  }
 
-    let componenteComentario
-
-    if(comentando) {
-      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
-    }
-
-  return(
+  return (
     <PostContainer>
       <PostHeader>
-        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <UserPhoto src={props.fotoUsuario} alt={"Imagem do usuario"} />
         <p>{props.nomeUsuario}</p>
       </PostHeader>
 
-      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+      <PostPhoto src={props.fotoPost} alt={"Imagem do post"} />
 
       <PostFooter>
         <IconeComContador
@@ -122,8 +110,7 @@ function Post(props){
       </PostFooter>
       {componenteComentario}
     </PostContainer>
-  )
+  );
 }
 
-
-export default Post
+export default Post;
