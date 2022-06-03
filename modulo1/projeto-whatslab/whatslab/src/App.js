688126450e35ae897f5a85805642bd6601719b7e
Pedro Henrique Durand Reis <webpedrod@gmail.com>
2022-06-03 11:09:38 -0300

    feat: lista de mensagens criada, envio funcionando

--- modulo1/projeto-whatslab/whatslab/src/App.js
+++ modulo1/projeto-whatslab/whatslab/src/App.js
@@ -1,46 +1,15 @@
-import styled from "styled-components"
-import { Messages } from "./components/Message/Message"
+import styled from "styled-components";
+import { Messages } from "./components/Message/Message";
 
 const BodyContainer = styled.div`
-display: flex;
-justify-content: center;
-`
-
-const MessagerContainer = styled.div`
-  border: 1px black solid;
-  min-width: 400px;
-  width: 80vh;
-  height: 100vh;
-`
-const InputContainer = styled.div `
- position: absolute;
- bottom: 0;
- max-width: 80vh;
- display: flex;
-`
-const InputUser = styled.input`
-width: 15vh;
-`
-
-const InputMessage = styled.input`
-width:55vh;
-`
-
-const SendButton = styled.button`
-width: 8vh;
-`
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <BodyContainer>
-    <MessagerContainer>
-      <Messages></Messages>
-    <InputContainer>
-    <InputUser placeholder="Usuário"></InputUser>
-    <InputMessage placeholder="Mensagem"></InputMessage>
-    <SendButton>Enviar</SendButton>
-    </InputContainer>
-    </MessagerContainer>
+      <Messages />
     </BodyContainer>
   );
 }
--- modulo1/projeto-whatslab/whatslab/src/components/Message/Message.js
+++ modulo1/projeto-whatslab/whatslab/src/components/Message/Message.js
@@ -1,24 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const MessageBox = styled.div`
-display: flex;
-flex-direction: row;
-`
-const MessageUser = styled.p`
-font-weight: bold;
-`
-const MessageText = styled.p`
-font-weight: normal;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  margin-left: 20px;
+  width: 100%;
+  height: 96vh;
+`;
 
-const Messages = ()=> {
+const MessagerContainer = styled.div`
+  border: 1px black solid;
+  min-width: 400px;
+  width: 80vh;
+  height: 100vh;
+`;
+
+const InputContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  max-width: 80vh;
+  display: flex;
+`;
+
+const InputUser = styled.input`
+  width: 15vh;
+`;
+
+const InputMessage = styled.input`
+  width: 55vh;
+`;
+
+const SendButton = styled.button`
+  width: 8vh;
+`;
+
+const MessageLine = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const BoldUser = styled.span`
+  font-weight: 600;
+  margin-right: 4px;
+  margin-bottom: 10px;
+`;
+
+const Messages = () => {
+  const [sendMessage, setSendMessage] = useState([]);
+
+  const [valorInputUser, setvalorInputUser] = useState("");
+  const [valorInputMessage, setvalorInputMessage] = useState("");
+
+  const addMessage = () => {
+    const addedMessage = {
+      user: valorInputUser,
+      message: valorInputMessage,
+    };
+    const sendMessages = [...sendMessage, addedMessage];
+    setSendMessage(sendMessages);
+  };
+
+  const onChangeInputUser = (event) => {
+    setvalorInputUser(event.target.value);
+  };
+
+  const onChangeInputMessage = (event) => {
+    setvalorInputMessage(event.target.value);
+  };
+  const messageList = sendMessage.map((message, index) => {
     return (
-        <MessageBox>
-        <MessageUser>Pedro:</MessageUser>
-        <MessageText>Olá</MessageText>
-        </MessageBox>
-    )
-}
+      <MessageLine key={index}>
+        <BoldUser>{message.user}:</BoldUser> {message.message}
+      </MessageLine>
+    );
+  });
 
-export {Messages}
+  return (
+    <MessagerContainer>
+      <MessageBox>{messageList}</MessageBox>
+
+      <InputContainer>
+        <InputUser
+          placeholder="Usuário"
+          value={valorInputUser}
+          onChange={onChangeInputUser}
+        ></InputUser>
+        <InputMessage
+          placeholder="Mensagem"
+          value={valorInputMessage}
+          onChange={onChangeInputMessage}
+        ></InputMessage>
+        <SendButton onClick={addMessage}>Enviar</SendButton>
+      </InputContainer>
+    </MessagerContainer>
+  );
+};
+
+export { Messages };
