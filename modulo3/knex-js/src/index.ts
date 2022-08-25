4921fe3d1abf1eb3908398291bc6136c1ab73bbe
Colaborador <colaborador@N094693.local>
2022-08-25 14:12:45 -0300

    commit

--- modulo3/knex-js/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-import createActor from "./endpoints/createActor";
-import getAllActors from "./endpoints/getAllActores";
-
-app.get("/atores", getAllActors);
-
-app.post("/atores", createActor);
