20e45d388cfe306ac373d1102883cf355af7ec03
Colaborador <colaborador@N094693.local>
2022-09-25 21:20:41 -0300

    user roles e criptografia de senha

--- modulo3/autentication/src/endpoints/createUser.ts
+++ modulo3/autentication/src/endpoints/createUser.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import connection from "../connection";
 import Authenticator from "../services/authenticator";
+import HashManager from "../services/hashManager";
 import { IdGenerator } from "../services/idGenerator";
 import { authenticationData, user } from "../types";
 
@@ -9,7 +10,7 @@ export default async function createUser(
   res: Response
 ): Promise<void> {
   try {
-    const { name, nickname, email, password } = req.body;
+    const { name, nickname, email, password, role } = req.body;
 
     if (!name || !nickname || !email || !password) {
       res.statusCode = 422;
@@ -25,19 +26,30 @@ export default async function createUser(
       throw new Error("Email já cadastrado");
     }
 
-    const id: string = new IdGenerator().generateId();
+    if (role && role !== "admin" && role !== "normal") {
+      res.statusCode = 409;
+      throw new Error(
+        "Insira o role do user: admin ou normal, deixe vazio para normal"
+      );
+    }
 
-    const newUser: user = { id, name, nickname, email, password };
+    const id: string = new IdGenerator().generateId();
+    const hash = await new HashManager().hash(password);
+
+    const newUser: user = { id, name, nickname, email, password: hash, role };
 
     await connection("to_do_list_users").insert(newUser);
 
     const payload: authenticationData = {
       id: newUser.id,
+      role: newUser.role,
     };
 
     const token = new Authenticator().generateToken(payload);
 
-    res.status(201).send({ token });
+    res
+      .status(201)
+      .send({ newUser: { id, name, nickname, email, role }, token });
   } catch (error: any) {
     if (res.statusCode === 200) {
       res.status(500).send({ message: "Internal server error" });
--- modulo3/autentication/src/endpoints/login.ts
+++ modulo3/autentication/src/endpoints/login.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import connection from "../connection";
 import Authenticator from "../services/authenticator";
+import HashManager from "../services/hashManager";
 import { authenticationData } from "../types";
 
 export default async function login(req: Request, res: Response) {
@@ -11,12 +12,21 @@ export default async function login(req: Request, res: Response) {
     }
     const [user] = await connection("to_do_list_users").where({ email });
 
-    if (!user || user.password !== password) {
+    const correctPassword = await new HashManager().compare(
+      password,
+      user.password
+    );
+
+    if (!correctPassword) {
+      throw new Error("senha incorreta");
+    }
+    if (!user) {
       throw new Error("Credenciais de acesso inválidas!");
     }
 
     const payload: authenticationData = {
       id: user.id,
+      role: user.role,
     };
 
     const token = new Authenticator().generateToken(payload);
@@ -24,6 +34,6 @@ export default async function login(req: Request, res: Response) {
     res.status(201).send({ token });
   } catch (error: any) {
     console.log(error);
-    res.status(500).send({ message: error });
+    res.status(500).send(error);
   }
 }
