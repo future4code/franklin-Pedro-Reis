5f4b3e3d2805bdeb08e3abe2062aa5c8d5a633b2
Colaborador <colaborador@N094693.local>
2022-09-27 12:29:47 -0300

    refactor

--- rodada-cases/Pizzaria/backend/src/business/UserBusiness.ts
+++ rodada-cases/Pizzaria/backend/src/business/UserBusiness.ts
@@ -2,12 +2,15 @@ export default class UserBusiness {
     public signup = async (input: any) => {
         const name = input.name;
         const email = input.email;
-        const whatsapp= input.whatsapp;
-        const password= input.password;
-        const cep= input.cep;
-        const street= input.street;
+        const whatsapp = input.whatsapp;
+        const password = input.password;
+        const cep = input.cep;
+        const street = input.street;
         const district= input.district;
-        const number= input.number;
+        const number = input.number;
         const reference= input.referenc;
+        if(!name) {
+            throw new Error("Favor inserir nome");
+        }
     }
 }
