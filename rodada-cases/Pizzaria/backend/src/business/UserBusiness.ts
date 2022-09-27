e6f2596228c147e402199540ce295fbda8a3b5fc
Colaborador <colaborador@N094693.local>
2022-09-27 12:24:55 -0300

    feat/criacao de userBusiness e userController

---
+++ rodada-cases/Pizzaria/backend/src/business/UserBusiness.ts
@@ -0,0 +1,13 @@
+export default class UserBusiness {
+    public signup = async (input: any) => {
+        const name = input.name;
+        const email = input.email;
+        const whatsapp= input.whatsapp;
+        const password= input.password;
+        const cep= input.cep;
+        const street= input.street;
+        const district= input.district;
+        const number= input.number;
+        const reference= input.referenc;
+    }
+}
---
+++ rodada-cases/Pizzaria/backend/src/controller/UserController.ts
@@ -0,0 +1,25 @@
+import {Request, Response} from 'express'
+import UserBusiness from '../business/UserBusiness'
+
+export default class UserController {
+    public signup =  async (req: Request, res: Response) => {
+        try {
+            const input: any ={
+                name: req.body.name,
+                email: req.body.email,
+                whatsapp: req.body.whatsapp,
+                password: req.body.password,
+                cep: req.body.cep,
+                street: req.body.street,
+                district: req.body.district,
+                number: req.body.number,
+                reference: req.body.reference
+            }
+            const userBusiness = new UserBusiness()
+            const response = await userBusiness.signup(input)
+            res.status(201).send(response)
+        } catch (error) {
+            res.status(400).send({message: error.message})
+        }
+    }
+}
