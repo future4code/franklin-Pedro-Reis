0f48753b0c92f9085668830e08efe1790054a837
Colaborador <colaborador@N094693.local>
2022-10-04 23:41:48 -0300

    feat/ arquivos iniciais do front end

---
+++ pizzaria/frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  VStack,
+  Grid,
+  theme,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Text>Pizzaria</Text>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
