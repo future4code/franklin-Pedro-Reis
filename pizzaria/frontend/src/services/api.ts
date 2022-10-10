2de3c5c395613cc5e4330f08419429b54e4f870c
Colaborador <colaborador@N094693.local>
2022-10-10 13:27:56 -0300

    reafactor/atualizacao da baseurl

--- pizzaria/frontend/src/services/api.ts
+++ pizzaria/frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3003",
+  baseURL: "https://appdepedidos.herokuapp.com",
 });
 
 instance.interceptors.request.use(
