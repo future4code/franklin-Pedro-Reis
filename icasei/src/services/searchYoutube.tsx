a7544841e9d6354ae1b7276689b99ce45915351f
Colaborador <colaborador@N094693.local>
2022-10-14 01:01:21 -0300

    feat/ criacao de chamada da api para pegar detalhes de um video especifico

--- icasei/src/services/searchYoutube.tsx
+++ icasei/src/services/searchYoutube.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const URL = "https://www.googleapis.com/youtube/v3/search";
-const KEY = "AIzaSyDjkNStOvJEjqefxy8SWLWXs5e8RMsypS8";
+const SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
+const VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos";
+const KEY = "AIzaSyASOhNPt1jMcSmRbOwcxRbF1Dr25ruI_to";
 
 export const searchYoutube = async (
   keyword: string,
@@ -17,7 +18,22 @@ export const searchYoutube = async (
   };
 
   try {
-    const response = await axios.get(URL, { params: params });
+    const response = await axios.get(SEARCH_URL, { params: params });
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const loadYoutubeVideo = async (videoId: string | undefined) => {
+  const params = {
+    id: videoId,
+    part: "snippet,statistics",
+    key: KEY,
+  };
+
+  try {
+    const response = await axios.get(VIDEO_URL, { params: params });
     return response;
   } catch (error) {
     console.error(error);
